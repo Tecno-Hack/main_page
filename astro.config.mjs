0d97bbe78ee9c260290bc46ae7536edcab2dfbd8
Cesar Luis Aranda <cesarluis.aranda@gmail.com>
2023-05-22 21:23:28 -0400

    primer commit

---
+++ astro.config.mjs
@@ -0,0 +1,21 @@
+import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
+import tailwind from "@astrojs/tailwind";
+
+import image from "@astrojs/image";
+
+// https://astro.build/config
+import netlify from "@astrojs/netlify/functions";
+
+// https://astro.build/config
+export default defineConfig({
+  site: 'https://astro-modern-personal-website.netlify.app',
+  integrations: [mdx(), sitemap(), tailwind(), image({
+    serviceEntryPoint: '@astrojs/image/sharp',
+    cacheDir: "./.cache/image",
+    logLevel: 'debug'
+  })],
+  output: "server",
+  adapter: netlify()
+});
